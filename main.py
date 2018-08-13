a11a4752937fc98fd386a33dcd775d580f8a5356
majorz <zahari@resin.io>
2018-08-13 16:27:18 +0300

    Add and activate connection

--- main.py
+++ main.py
@@ -1,3 +1,70 @@
-import signal
+import uuid
+import os
+import time
 
-signal.pause()
+import NetworkManager
+
+ssid = os.getenv('WIFI_SSID')
+password = os.getenv('WIFI_PASSWORD')
+interface = os.getenv('WIFI_INTERFACE')
+
+if not ssid:
+    print('WIFI_SSID not exported')
+    exit()
+
+if not password:
+    print('WIFI_PASSWORD not exported')
+    exit()
+
+if not interface:
+    print('WIFI_INTERFACE not exported')
+    exit()
+
+device = NetworkManager.NetworkManager.GetDeviceByIpIface(interface)
+print('Device:', device.object_path)
+
+start_sleep = 15
+print('Sleeping for', start_sleep, 'seconds')
+time.sleep(start_sleep)
+
+settings = {
+    '802-11-wireless': {
+        'mode': 'infrastructure',
+        'security': '802-11-wireless-security',
+        'ssid': ssid,
+    },
+    '802-11-wireless-security': {
+        'auth-alg': 'open',
+        'key-mgmt': 'wpa-psk',
+        'psk': password,
+    },
+    'connection': {
+        'id': 'jordan',
+        'type': '802-11-wireless',
+        'uuid': str(uuid.uuid4())
+    },
+    'ipv4': {
+        'method': 'auto'
+    },
+    'ipv6': {
+        'method': 'auto'
+    }
+}
+
+connection = NetworkManager.Settings.AddConnection(settings)
+
+print('Connection added:', connection.object_path)
+
+add_sleep = 5
+print('Sleeping for', add_sleep, 'seconds')
+time.sleep(add_sleep)
+
+active_connection = NetworkManager.NetworkManager.ActivateConnection(connection, device, '/')
+
+print('Active connection:', active_connection.object_path)
+
+activate_sleep = 20
+print('Sleeping for', activate_sleep, 'seconds')
+time.sleep(activate_sleep)
+
+print('State:', active_connection.State)
